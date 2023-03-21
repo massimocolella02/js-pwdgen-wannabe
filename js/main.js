244e088d27a9e1e38416e7cd6257b1ea8e422096
massimocolella02 <massimo-colella@outlook.it>
2023-03-21 19:46:34 +0100

    Esercizio Completato

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let nome = prompt('Inserire Nome');
+let cognome = prompt('Inserire Cognome');
+let colore = prompt('Inserire Colore preferito');
+
+let password = nome + cognome + colore
+
+
+alert(
+    'La tua password è ' + password
+    )
